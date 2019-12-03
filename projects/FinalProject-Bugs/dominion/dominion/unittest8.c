e275efd41f2bbffcd346c52d6764dff0f6aa520f
huangwei0 <40774443+huangwei0@users.noreply.github.com>
2019-12-03 14:48:05 -0800

    Update unittest8.c

--- projects/FinalProject-Bugs/dominion/dominion/unittest8.c
+++ projects/FinalProject-Bugs/dominion/dominion/unittest8.c
@@ -1,77 +1,71 @@
 #include "dominion.h"
 #include "dominion_helpers.h"
+#include "rngs.h"
 #include <string.h>
 #include <stdio.h>
-#include <assert.h>
-#include "rngs.h"
+#include <math.h>
 #include <stdlib.h>
+#include <assert.h>
 
-int main()
+void main ()
 {
+	struct gameState state;
+	int i = 0;
 
-    int i, j, m;
-    int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
-    int seed = 1000;
-    int numPlayers = 2;
-    int player=0;
-	struct gameState state, test;
-	int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall };
-	int count1 = 0;
-	int count2 = 0;
+ 	printf("\n----------Start Testing Update Coins ----------\n");
 
 
-	initializeGame(numPlayers, k, seed, &state);
-  printf("\n----------Testing bonus----------\n");
-	memcpy(&test, &state, sizeof(struct gameState));
-	cardEffect(feast, choice1, choice2, choice3, &state, handpos, &bonus);
-	player = whoseTurn(&test);
-  if(test.coins+5 == state.coins)
-    printf("Passed where the feast was add coins.\n");
-  else
-  {
-    printf("Failed where the feast was not add coins.\n");
-  }
+	//COPPER
+	printf("\nTesting copper\n");
+	for (i = 0; i < 10; i++)
+		state.hand[0][i] = copper;
 
-  memcpy(&test, &state, sizeof(struct gameState));
-	cardEffect(baron, choice1, choice2, choice3, &state, handpos, &bonus);
-	player = whoseTurn(&test);
-  if(test.coins+4 == state.coins)
-    printf("Passed where the baron was add coins.\n");
-  else
-  {
-    printf("Failed where the baron was not add coins.\n");
-  }
+	updateCoins(0,&state, 0);
 
-  memcpy(&test, &state, sizeof(struct gameState));
-  cardEffect(minion, choice1, choice2, choice3, &state, handpos, &bonus);
-  player = whoseTurn(&test);
-  if(test.coins+2 == state.coins)
-    printf("Passed where the minion was add coins.\n");
-  else
-  {
-    printf("Failed where the minion was not add coins.\n");
-  }
-
-  memcpy(&test, &state, sizeof(struct gameState));
-  cardEffect(steward, choice1, choice2, choice3, &state, handpos, &bonus);
-  player = whoseTurn(&test);
-  if(test.coins+2 == state.coins)
-    printf("Passed where the minion was add coins.\n");
-  else
-  {
-    printf("Failed where the minion was not add coins.\n");
-  }
-
-  memcpy(&test, &state, sizeof(struct gameState));
-  cardEffect(tribute, choice1, choice2, choice3, &state, handpos, &bonus);
-  player = whoseTurn(&test);
-  if(test.coins+2 == state.coins)
-    printf("Passed where the tribute was add coins.\n");
-  else
-  {
-    printf("Failed where the tribute was not add coins.\n");
-  }
+	if(state.coins == 10)
+		printf("\nPassed where copper is %d/10\n", state.coins);
+	else
+		printf("\nFailed where copper is %d/10\n", state.coins);
 
 
-  printf("\n----------Testing bonus end----------\n");
+	//SILVER
+	printf("\nTesting silver\n");
+	for (i = 0; i < 10; i++)
+		state.hand[0][i] = silver;
+
+	updateCoins(0,&state, 0);
+
+	if(state.coins == 20)
+		printf("\nPassed where silver is %d/20\n", state.coins);
+	else
+		printf("\nFailed where silver is %d/20\n", state.coins);
+
+
+	//GOLD
+	printf("\nTesting gold\n");
+	for (i = 0; i < 10; i++)
+		state.hand[0][i] = gold;
+
+	updateCoins(0,&state, 0);
+
+	if(state.coins == 30)
+		printf("\nPassed where gold is %d/30\n", state.coins);
+	else
+		printf("\nFailed where gold is %d/30\n", state.coins);
+
+
+	//Bonus Copper
+	printf("\nTesting copper + 1 bonus\n");
+	for (i = 0; i < 10; i++)
+		state.hand[0][i] = copper;
+
+	updateCoins(0,&state, 1);
+
+	if(state.coins == 11)
+		printf("\nPassed where copper + 1 bonus is %d/11\n", state.coins);
+	else
+		printf("\nFailed where copper + 1 bonus is %d/11\n", state.coins);
+
+
+ 	printf("\n----------Start Testing Update Coins ----------\n");
 }
